f9eac005f036fe9e0ea7a0c19bd83cd98b138730
Igor M <iggorm@yahoo.com.br>
2021-02-19 12:38:19 -0300

    atualização blog_pessoal

--- spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
@@ -31,6 +33,10 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis()); // PEGA A DATA A PARTIR DO MOMENTO EM QUE PASSA UM DADO POR ESSA CLASSE.
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 	
 	public long getId() {
 		return id;
@@ -56,6 +62,13 @@ public class Postagem {
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
 	
 }
 
